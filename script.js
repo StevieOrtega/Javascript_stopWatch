6e1e13c00b0a1460cd3fd57b03bdad625b1a9b93
Steven Ortega <56889510+StevieOrtega@users.noreply.github.com>
2023-01-30 12:48:11 -0800

    js varables added

---
+++ script.js
@@ -0,0 +1,11 @@
+const sec1 = document.getElementById("second1");
+const sec2 = document.getElementById("second2");
+const min1 = document.getElementById("minute1");
+const min2 = document.getElementById("minute2");
+const startBtn = document.getElementById("start-btn");
+
+sec2.textContent = 4;
+min1.textContent = 3;
+min2.textContent = 6;
+
+// add function to start changing the minutes and seconds
