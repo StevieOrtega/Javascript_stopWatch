501d4b5633f6ea57ee1b4fc39cd804fb3b84226f
Steven Ortega <56889510+StevieOrtega@users.noreply.github.com>
2023-01-30 16:55:07 -0800

    startbtn function created

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const sec1 = document.getElementById("second1");
+//const sec1 = document.getElementById("second1");
 const sec2 = document.getElementById("second2");
 const min1 = document.getElementById("minute1");
 const min2 = document.getElementById("minute2");
 const startBtn = document.getElementById("start-btn");
-
-sec2.textContent = 4;
-min1.textContent = 3;
-min2.textContent = 6;
+const stopBtn = document.getElementById("stop-btn");
+var seconds = 0;
+var minutes = 0;
 
 // add function to start changing the minutes and seconds
+startBtn.addEventListener("click", function () {
+  startBtn.style.background = "red";
+  setInterval(function () {
+    seconds++;
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+    console.log(`${minutes} : ${seconds}`);
+  }, 1);
+});
+//create stop button function
