8442bfe10e45bba3baa38f9dddce74705b1f72b9
Steven Ortega <56889510+StevieOrtega@users.noreply.github.com>
2023-01-31 10:37:31 -0800

    js done

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-//const sec1 = document.getElementById("second1");
-const sec2 = document.getElementById("second2");
-const min1 = document.getElementById("minute1");
-const min2 = document.getElementById("minute2");
+"use strict";
 const startBtn = document.getElementById("start-btn");
 const stopBtn = document.getElementById("stop-btn");
+const resetBtn = document.getElementById("reset-btn");
+const minDisplay = document.getElementById("minute");
+const secDisplay = document.getElementById("second");
+
 var seconds = 0;
 var minutes = 0;
-//testign
-// add function to start changing the minutes and seconds
-startBtn.addEventListener("click", function () {
-  startBtn.style.background = "red";
-  setInterval(function () {
-    seconds++;
-    if (seconds === 60) {
-      minutes++;
-      seconds = 0;
-    }
-    console.log(`${minutes} : ${seconds}`);
-  }, 1);
-});
-//create stop button function
+var timer;
+
+const stopWatchFeats = function () {
+  startBtn.addEventListener("click", function () {
+    timer = setInterval(function () {
+      secDisplay.textContent = seconds;
+      minDisplay.textContent = minutes;
+      seconds++;
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+    }, 1000);
+  });
+  //create stop button function
+  stopBtn.addEventListener("click", function () {
+    clearInterval(timer);
+  });
+
+  resetBtn.addEventListener("click", function () {
+    seconds = minutes = 0;
+    minDisplay.textContent = seconds;
+    secDisplay.textContent = minutes;
+    clearInterval(timer);
+  });
+};
+stopWatchFeats();
